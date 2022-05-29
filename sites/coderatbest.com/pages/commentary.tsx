8556f954dd7e3766affbaf96c36a984912431652
al3xdiaz <alexleonel96@hotmail.com>
2022-05-29 15:31:24 -0600

    feat: commentary page

---
+++ sites/coderatbest.com/pages/commentary.tsx
@@ -0,0 +1,11 @@
+
+import type { NextPage } from 'next'
+const Commentary = () => {
+    return (
+        <div>
+            <h1>Commentary</h1>
+        </div>
+    );
+}
+
+export default Commentary;
