98e39e422efbfca034c8f79a6773789fedff8ee4
al3xdiaz <alexleonel22@hotmail.com>
2022-07-22 23:46:25 -0600

    feat: next links

--- portafolio/components/navbar/index.tsx
+++ portafolio/components/navbar/index.tsx
@@ -1,15 +1,28 @@
-
+import Link from "next/link"
 export const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="navbar__logo">
-                <a href="#">Alex Diaz</a>
+                <Link href="/">
+                    <a>Alex Diaz</a>
+                </Link>
             </div>
             <menu>
-                <a href="#">Cursos</a>
-                <a href="#">Habilidades</a>
-                <a href="#">pasatiempos</a>
-                <a href="#">Contacto</a>
+                <Link href="/">
+                    <a>Home</a>
+                </Link>
+                <Link href="/cursos">
+                    <a>Cursos</a>
+                </Link>
+                <Link href="/habilidades">
+                    <a>Habilidades</a>
+                </Link>
+                <Link href="/pasatiempos">
+                    <a>Pasatiempos</a>
+                </Link>
+                <Link href="/contacto">
+                    <a>Contacto</a>
+                </Link>
             </menu>
         </nav>
     )
