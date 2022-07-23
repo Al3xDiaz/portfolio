9fcb595519853e2c004d770db1e60cbea598336c
al3xdiaz <alexleonel22@hotmail.com>
2022-07-22 20:29:33 -0600

    feat: carrusel

--- al3xdiaz/pages/index.tsx
+++ al3xdiaz/pages/index.tsx
@@ -2,11 +2,12 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import  Carrusel from '@/components/carrusel/carrusel'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      hola mundo
+      <Carrusel />
     </div>
   )
 }
