a43cf8e706a1b61271be86a5ea4ccffb1c2d7626
al3xdiaz <alexleonel22@hotmail.com>
2022-07-22 23:08:34 -0600

    fix: hibridate

--- portafolio/components/footer/index.tsx
+++ portafolio/components/footer/index.tsx
@@ -1,1 +1,32 @@
-export  { default as  Footer } from './Footer'; 
+import React from 'react';
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <div  className="myself" >
+                <h4>I am a simple human being</h4>
+                <div>
+                    I am a simple human being who lives in a simple world.
+                </div>
+                <div>
+                    <h4>Social</h4>
+                    
+                </div>
+            </div>
+            <div className="about">
+                <h4>Explore</h4>
+                <ul>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                    <li>
+                        <a href="#">Privacy and Terms</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    );
+}
+export default Footer;
