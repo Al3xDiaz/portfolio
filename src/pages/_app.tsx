8b2b62f0a3fac5f98b3e78ced4a50f417897e4a7
al3xdiaz <alexleonel96@hotmail.com>
2024-06-07 00:35:41 -0600

    [fix]  build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,25 @@
 import '../styles/globals.css';
-import App,{ AppContext, AppInitialProps, AppProps } from 'next/app';
+import type { AppProps } from 'next/app';
+import Layout from '@/src/components/layaut';
 import siteContex from '@/src/context/siteContext';
-import { useEffect, useMemo, useReducer } from 'react';
+import { useCallback, useEffect, useMemo, useReducer, useRef } from 'react';
 import { initialState, reducer } from '@/src/reducers/useSiteReducer';
 import { SiteService } from '@/src/services';
+import { useRouter } from 'next/router';
+import { PacmanLoader } from '@/src/components/loader/packman';
 import getConfig from "next/config";
-import {AchievementsService} from '@/src/services/timelineService';
-import axios from 'axios';
 
 const { publicRuntimeConfig } = getConfig();
-const API_URL = publicRuntimeConfig.API_URL;
+const baseURL = publicRuntimeConfig.API_URL;
 
-interface AppOwnProps extends AppProps{
-}
-function CustomApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
+
+	const router = useRouter()
 
 	const [state,dispatch]= useReducer(reducer,initialState);
 	useMemo(()=>{
 		return {state,dispatch}
-	},[state,dispatch]);
+	},[state,dispatch])
 
 	let content = <Component {...pageProps} />
 	return (
@@ -33,30 +34,5 @@ function CustomApp({ Component, pageProps }: AppProps) {
 		</siteContex.Provider>
 	)
 }
-CustomApp.getInitialProps = async(context:AppContext):Promise<AppOwnProps|AppInitialProps>=>{
-  const {query: params} = context.router
-  const ctx = await App.getInitialProps(context);
-  const username = params && params["username"]?.toString() || ""
-  const srcSite = new SiteService(API_URL)
-  const srcAchivement = new AchievementsService(API_URL)
-  const user = await srcSite.getSlugName(username);
-  console.log("user: "+ username,user)
-  const timeline = await srcAchivement.list(username);
-  const response =await axios.get(`${API_URL}/courses`,{
-    params:{
-      username,
-      limit:3,
-    }
-  })
-  const courses = response.data
-  return {
-    ...ctx,
-    pageProps:{
-      user,
-      timeline,
-      courses
-    }
-  }
-}
 
-export default CustomApp;
+export default App;
